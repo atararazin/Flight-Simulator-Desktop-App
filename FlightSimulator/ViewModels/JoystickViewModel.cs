33e057674d65227d4b14c821ed4650c725fe9d30
Yossi Weizman <yossi.weizman@gmail.com>
2019-04-12 08:48:51 +0300

    The manual control send commands to the simulator

--- FlightSimulator/ViewModels/JoystickViewModel.cs
+++ FlightSimulator/ViewModels/JoystickViewModel.cs
@@ -3,10 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FlightSimulator.Model;
 
 namespace FlightSimulator.ViewModels
 {
-    class JoystickViewModel
+    class JoystickViewModel : BaseNotify
     {
+        private ManualFlightModel model;
+        //private double _throttleVal;
+        //private double _rudderVal;
+
+        public JoystickViewModel()
+        {
+            this.model = new ManualFlightModel();
+        }
+
+        public double ThrottleVal
+        {
+            //get { return this._throttleVal; }
+            get { return model.ThrottleValue; }
+            set
+            {
+                //this._throttleVal = value;
+                model.ThrottleValue = value;
+                Console.WriteLine(value);
+            }
+        }
+
+        public double RudderVal
+        {
+            //get { return this._rudderVal; }
+            get { return model.RudderValue; }
+            set
+            {
+                //this._rudderVal = value;
+                model.RudderValue = value;
+                Console.WriteLine(value);
+            }
+        }
+
+
+
+
     }
 }
