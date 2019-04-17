28b86c47b0c7b5748e15e48b61bfb15c27ebb8f8
Yossi Weizman <yossi.weizman@gmail.com>
2019-04-17 10:28:47 +0300

    Started working on flight board

---
+++ FlightSimulator/Model/FlightBoardModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulator.Model
+{
+    class FlightBoardModel
+    {
+        private double _lon;
+        private double _lat;
+        private InformationServer srv;
+
+        public FlightBoardModel()
+        {
+            //this.srv = new InformationServer();
+        }
+
+
+        public double Lon
+        {
+            get { return this._lon; }
+            set { }
+        }
+        
+        public double Lat
+        {
+            get { return this._lat; }
+            set { }
+        }
+
+
+    }
+}
--- FlightSimulator/ViewModels/FlightBoardViewModel.cs
+++ FlightSimulator/ViewModels/FlightBoardViewModel.cs
@@ -4,11 +4,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FlightSimulator.Model;
 
 namespace FlightSimulator.ViewModels
 {
     public class FlightBoardViewModel : BaseNotify
     {
+        private FlightBoardModel model;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public FlightBoardViewModel()
+        {
+            this.model = new FlightBoardModel();
+        }
 
         public double Lon
         {
