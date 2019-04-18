07b990f58502c004a4fdd15cf0e7dbffcb0675f9
Yossi Weizman <yossi.weizman@gmail.com>
2019-04-18 20:08:32 +0300

    Continue flight board

--- FlightSimulator/ViewModels/FlightBoardViewModel.cs
+++ FlightSimulator/ViewModels/FlightBoardViewModel.cs
@@ -5,19 +5,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FlightSimulator.Model;
+using System.ComponentModel;
 
 namespace FlightSimulator.ViewModels
 {
     public class FlightBoardViewModel : BaseNotify
     {
         private FlightBoardModel model;
+        private double _lon;
+        private double _lat;
 
         /// <summary>
         /// Constructor
         /// </summary>
         public FlightBoardViewModel()
         {
-             this.model = new FlightBoardModel();
+            this.model = new FlightBoardModel();
+            this.model.PropertyChanged += Model_PropertyChanged;
         }
 
         public double Lon
@@ -26,6 +30,12 @@ namespace FlightSimulator.ViewModels
             {
                 return this.model.Lon;
             }
+            set
+            {
+                this._lon = value;
+                NotifyPropertyChanged("Lon");
+            }
+
         }
 
         public double Lat
@@ -33,6 +43,23 @@ namespace FlightSimulator.ViewModels
             get
             {
                 return this.model.Lat;
+            }
+            set
+            {
+                this._lat = value;
+                NotifyPropertyChanged("Lat");
+            }
+        }
+
+        private void Model_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName.Equals("Lat"))
+            {
+                Lat = model.Lat;
+            }
+            if (e.PropertyName.Equals("Lon"))
+            {
+                Lon = model.Lon;
             }
         }
     }
--- FlightSimulator/Views/FlightBoard.xaml.cs
+++ FlightSimulator/Views/FlightBoard.xaml.cs
@@ -51,7 +51,8 @@ namespace FlightSimulator.Views
             if(e.PropertyName.Equals("Lat") || e.PropertyName.Equals("Lon"))
             {
                 //Point p1 = new Point(0,0);            // Fill here!
-                Point p1 = new Point(vm.Lon, vm.Lat);
+                //Point p1 = new Point(vm.Lon, vm.Lat);
+                Point p1 = new Point(vm.Lat, vm.Lon);
                 planeLocations.AppendAsync(Dispatcher, p1);
             }
         }
